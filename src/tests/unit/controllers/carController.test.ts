2c2d49c3ed6e09a5fc7bcd62c79d857aa49e377e
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-11 17:28:40 -0300

    faet: implementação dos testes da camada CarController para a requisição GET/cars

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import { Request, Response } from 'express';
 import CarService from '../../../services/CarService';
-import { carMock, carMockWithId } from '../../mocks/carMocks';
+import { allCarsMockWithId, carMock, carMockWithId } from '../../mocks/carMocks';
 import CarController from '../../../controllers/CarController';
 const { expect } = chai;
 
@@ -15,6 +15,8 @@ describe('Testa o controller CarController', () => {
   before(async () => {
     sinon
       .stub(carController._service, 'create').resolves(carMockWithId)
+    sinon
+      .stub(carController._service, 'read').resolves(allCarsMockWithId)
 
     res.status = sinon.stub().returns(res)
     res.json = sinon.stub().returns(res)
@@ -24,13 +26,22 @@ describe('Testa o controller CarController', () => {
     sinon.restore()
   })
 
-  describe('quando é criado um novo Car', () => {
+  describe('ao criar um novo carro', () => {
     it('com sucesso, é retornado um status 201 com o objeto criado', async () => {
       req.body = carMock
       await carController.create(req, res)
 
       expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
+    })
+  })
+
+  describe('ao pesquisar todos os carros', () => {
+    it('com sucesso, é retornado um status 200', async () => {
+      await carController.read(req, res)
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(allCarsMockWithId)).to.be.true
     })
   })
 });
