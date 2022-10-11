ffa75be87c40c60dd67687bb94682ae1bff61d3c
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-11 10:47:46 -0300

    fix: correção do nome da rota /car para /cars

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -5,6 +5,6 @@ const router = Router();
 
 const carController = new CarController();
 
-router.post('/car', (req, res) => carController.create(req, res));
+router.post('/cars', (req, res) => carController.create(req, res));
 
 export default router;
