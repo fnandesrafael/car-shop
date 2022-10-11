549d3b6cd92a27d56555d14278f981b2f2c2b097
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-11 15:23:32 -0300

    refact: adição de casos extras aos testes da model Car

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -13,7 +13,9 @@ describe('Testa a model Car', () => {
     sinon
       .stub(Model, 'find').resolves(allCarsMockWithId)
     sinon
-      .stub(Model, 'findById').resolves(carMockWithId)
+      .stub(Model, 'findById')
+        .onCall(0).resolves(carMockWithId)
+        .onCall(1).resolves(null)
   });
 
   after(()=>{
@@ -41,6 +43,11 @@ describe('Testa a model Car', () => {
       const sut = await new Car().readOne('4edd40c86762e0fb12000003')
 
       expect(sut).to.be.deep.equal(carMockWithId)
+    })
+    it('com sucesso, mas o documento não existe, é retornado nulo', async () => {
+      const sut = await new Car().readOne('4edd40c86762e0fb12000004')
+
+      expect(sut).to.be.deep.equal(null)
     })
   })
 });
