89daa6ec09ace4b3d4c5264b040a21e8e3937ba5
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-12 19:10:27 -0300

    feat: implementação dos testes do controller MotorcycleController no método create

---
+++ src/tests/unit/controllers/motorcycleController.test.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import { Request, Response } from "express";
+import sinon, { SinonStub } from 'sinon'
+import { motorcycleMock, motorcycleMockWithId } from "../../mocks/motorcycleMocks";
+
+const motorcycleController = new MotorcycleController()
+
+describe('Testa a camada MotorcycleController', () => {
+  const req = {} as Request
+  const res = {} as Response
+  
+  beforeEach(() => {
+    sinon.stub(motorcycleController.motorcycleService, 'create').resolves(motorcycleMockWithId)
+    
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub().returns(res)
+  })
+
+  afterEach(() => {
+    sinon.restore
+  })
+  
+  describe('ao criar uma nova moto', () => {
+    it('com sucesso, é retornado um status 201', async () => {
+      req.body = motorcycleMock
+      await motorcycleController.create(req, res)
+
+      expect((res.status as SinonStub).calledWith(201)).to.be.true
+    });
+
+    it('com sucesso, é retornado no corpo da requisição, o documento criado', async () => {
+      req.body = motorcycleMock
+      await motorcycleController.create(req, res)
+
+      expect((res.json as SinonStub).calledWith(motorcycleMockWithId)).to.be.true
+    });
+  });
+});
