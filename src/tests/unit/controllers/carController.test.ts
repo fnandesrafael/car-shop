d3b912fcecdf008b47a2f3f0c263dfcca29ecc0a
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-12 10:04:53 -0300

    feat: implementação dos testes do método readOne do controller CarController

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -14,6 +14,7 @@ describe('Testa o controller CarController', () => {
   beforeEach(() => {
     sinon.stub(carController._service, 'create').resolves(carMockWithId)
     sinon.stub(carController._service, 'read').resolves(allCarsMockWithId)
+    sinon.stub(carController._service, 'readOne').resolves(carMockWithId)
 
     res.status = sinon.stub().returns(res)
     res.json = sinon.stub().returns(res)
@@ -23,12 +24,17 @@ describe('Testa o controller CarController', () => {
     sinon.restore()
   })
 
-  describe('ao criar um novo carro com sucesso', () => {
-    it('é retornado um status 201 com o objeto criado', async () => {
+  describe('ao criar um novo carro', () => {
+    it('com sucesso, é retornado um status 201', async () => {
       req.body = carMock
       await carController.create(req, res)
 
       expect((res.status as SinonStub).calledWith(201)).to.be.true;
+    });
+    it('com sucesso, é retornado no body da requisição, o objeto criado', async () => {
+      req.body = carMock
+      await carController.create(req, res)
+
       expect((res.json as SinonStub).calledWith(carMockWithId)).to.be.true;
     });
   })
@@ -39,6 +45,26 @@ describe('Testa o controller CarController', () => {
 
       expect((res.status as SinonStub).calledWith(200)).to.be.true;
       expect((res.json as SinonStub).calledWith(allCarsMockWithId)).to.be.true
+    });
+    it('com sucesso, é retornado no body da requisição, todos os carros do banco', async () => {
+      await carController.read(req, res)
+
+      expect((res.json as SinonStub).calledWith(allCarsMockWithId)).to.be.true
+    });
+  })
+
+  describe('ao pesquisar um carro específico', () => {
+    it('com sucesso, é retornado um status 200', async () => {
+      req.params = { id: '4edd40c86762e0fb12000003' }
+      await carController.readOne(req, res)
+
+      expect((res.status as SinonStub).calledWith(200)).to.be.true;
+    });
+    it('com sucesso, é retornado no body da requisição, o documento pesquisado', async () => {
+      req.params = { id: '4edd40c86762e0fb12000003' }
+      await carController.readOne(req, res)
+
+      expect((res.json as SinonStub).calledWith(carMockWithId)).to.be.true;
     });
   })
 });
