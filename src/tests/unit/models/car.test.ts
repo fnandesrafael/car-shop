eb9a1d6d9293a614cea53bb740cf77b8a56718c9
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-12 17:31:39 -0300

    feat: testes da model Car no método DELETE/cars/:id

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -17,6 +17,9 @@ describe('Testa a model Car', () => {
     sinon.stub(Model, 'findByIdAndUpdate')
       .onCall(0).resolves(carMockForUpdateWithId)
       .onCall(1).resolves(null)
+    sinon.stub(Model, 'findByIdAndRemove')
+      .onCall(0).resolves(carMockWithId)
+      .onCall(1).resolves(null)
   });
 
   after(() => {
@@ -65,5 +68,19 @@ describe('Testa a model Car', () => {
 
       expect(sut).to.be.equal(null)
     })
+  })
+
+  describe('ao apagar um documento específico', () => {
+    it('com sucesso, e o documento existe, é retornado o documento apagado', async () => {
+      const sut = await carModel.delete('4edd40c86762e0fb12000003')
+
+      expect(sut).to.be.equal(carMockWithId)
+    });
+
+    it('com sucesso, mas o documento não existe, é retornado nulo', async () => {
+      const sut = await carModel.delete('4edd40c86762e0fb12000004')
+
+      expect(sut).to.be.equal(null)
+    });
   })
 });
